4db2ec0d39c4ac60d2a6acf8c001634cfbd1a8f4
root <aykayem92@gmail.com>
2019-02-01 14:42:34 -0500

    switched to discord.py rewrite 1.0

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import csv
 import time
 
 BOT_PREFIX = "/"
-TOKEN = open("token.txt", 'r').read().replace("\n", '')
+TOKEN = open("token_test.txt", 'r').read().replace("\n", '')
 client = Bot(command_prefix = BOT_PREFIX)
 client.remove_command('help')
 
@@ -28,11 +28,18 @@ def get_games(id):
 
 
 def get_stores():
+<<<<<<< HEAD
+        url = "http://cheapshark.com/api/1.0/stores"
+        response = requests.get(url)
+        platforms = response.json()
+        return platforms
+=======
     url = "http://cheapshark.com/api/1.0/stores"
     response = requests.get(url)
     platforms = response.json()
     return platforms
 
+>>>>>>> 400bf0e7bc4845d519d753066de3450d34c937d0
 
 def parse_games(data):
     game_list = []
@@ -83,6 +90,17 @@ def csv_create(game_list):
 
 @client.command(pass_context=True, aliases = ['Deals_CSV', 'deals_Csv', 'Deals_csv', 'deals_CSV', 'DEALS_CSV'])
 async def deals_csv(ctx):
+<<<<<<< HEAD
+        author = ctx.message.author
+        channel = ctx.message.channel
+        id = 100
+        data = get_games(id)
+        game_list = parse_games(data)
+        filename = csv_create(game_list)
+        await channel.send("Your csv file is being sent to your DMs")
+        print("/deals_csv: " + filename)
+        await author.send(file=discord.File(filename))
+=======
     author = ctx.message.author
     id = 100
     data = get_games(id)
@@ -91,10 +109,23 @@ async def deals_csv(ctx):
     await client.say("Your csv file is being sent to your DMs")
     print("/deals_csv: " + filename)
     await client.send_file(author, filename)
+>>>>>>> 400bf0e7bc4845d519d753066de3450d34c937d0
 
 
 @client.command(pass_context=True, aliases = ['Deals_Spam', 'Deals_spam', 'deals_Spam', 'DEALS_SPAM'])
 async def deals_spam(ctx):
+<<<<<<< HEAD
+        id = 100
+        author = ctx.message.author
+        channel = ctx.message.channel
+        data = get_games(id)
+        game_list = parse_games(data)
+        print("/deals_spam")
+        await channel.send("Your game deals are being sent to your DMs right now")
+        for game in game_list:
+                await author.send("-" * 120 + "\nDeal Link: "+ game[11] +"\nTitle: " + game[0] + "\nStore: " + game[5] + "\nMSRP: " + game[6] + "\nSale Price: " + game[7] + "\nDiscount: " + game[8] + "\nDeal Rating: " + game[9])
+                time.sleep(1)
+=======
     id = 100
     author = ctx.message.author
     data = get_games(id)
@@ -104,9 +135,31 @@ async def deals_spam(ctx):
     for game in game_list:
         await client.send_message(author, "-" * 120 + "\nDeal Link: " + game[11] + "\nTitle: " + game[0] + "\nStore: " + game[5] + "\nMSRP: " + game[6] + "\nSale Price: " + game[7] + "\nDiscount: " + game[8] + "\nDeal Rating: " + game[9])
         time.sleep(1)
+>>>>>>> 400bf0e7bc4845d519d753066de3450d34c937d0
+
 
 @client.command(pass_context=True, aliases = ['Deals_Custom', 'Deals_custom', 'deals_Custom', 'DEALS_CUSTOM'])
 async def deals_custom(ctx, arg):
+<<<<<<< HEAD
+        author = ctx.message.author
+        channel = ctx.message.channel
+        arg = arg.lower()
+        stores = []
+        ids = []
+        platforms = get_stores()
+        for key in platforms:
+                stores.append(key['storeName'].lower())
+                ids.append(key['storeID'])
+        if arg in stores:
+                i = stores.index(arg)
+                id = ids[i]
+        data = get_games(id)
+        game_list = parse_games(data)
+        filename = csv_create(game_list)
+        await channel.send("Your custom csv file for " + arg + " is being sent to your DMs")
+        print(filename)
+        await author.send(file=discord.File(filename))
+=======
     author = ctx.message.author
     arg = arg.lower()
     stores = []
@@ -124,17 +177,36 @@ async def deals_custom(ctx, arg):
     await client.say("Your custom csv file for " + arg + " is being sent to your DMs")
     print(filename)
     await client.send_file(author, filename)
+>>>>>>> 400bf0e7bc4845d519d753066de3450d34c937d0
 
 
 @client.command(pass_context=True, aliases = ['ShowStores', 'Showstores', 'showstores', 'SHOWSTORES'])
 async def showStores(ctx):
+<<<<<<< HEAD
+        author = ctx.message.author
+        stores = []
+        platforms = get_stores()
+        for key in platforms:
+                stores.append(key['storeName'])
+        await author.send("These are all the stores that our bot can check for:\n" + stores[0] + "\n" + stores[1] + "\n"+ stores[2] + "\n" + stores[3] + "\n" + stores[4] + "\n" + stores[5] + "\n" + stores[6] + "\n" + stores[7] + "\n" + stores[8] + "\n" + stores[9] + "\n" + stores[10] + "\n" + stores[11] + "\n" + stores[12] + "\n" + stores[13] + "\n" + stores[14] + "\n" + stores[15] + "\n" + stores[16] + "\n" + stores[17] + "\n" + stores[18] + "\n" + stores[19] + "\n" + stores[20] + "\n" + stores[21] + "\n" + stores[22] + "\n" + stores[23] + "\n" + stores[24]) 
+
+
+@client.command(pass_context=True, aliases = ['Help', 'HELP'])
+async def help(ctx):
+        help = "You have asked for help!\nThis discord bot is capable of requesting the best game deals from a Public API across multiple platforms.\nCommands:\n`/deals_csv` - Sends detailed game deals information to DMs in csv format\n`/deals_spam` - Sends game deals to your DMs and includes a picture.\n`/deals_custom \"<store>\"` - Searches for specific store\n`/showStores` - Shows stores that can be used in custom search"
+        author = ctx.message.author
+        channel = ctx.message.channel
+        await channel.send(help) 
+=======
     author = ctx.message.author
     stores = []
     platforms = get_stores()
     for key in platforms:
         stores.append(key['storeName'])
     await client.send_message(author,"These are all the stores that our bot can check for:\n" + stores[0] + "\n" + stores[1] + "\n"+ stores[2] + "\n" + stores[3] + "\n" + stores[4] + "\n" + stores[5] + "\n" + stores[6] + "\n" + stores[7] + "\n" + stores[8] + "\n" + stores[9] + "\n" + stores[10] + "\n" + stores[11] + "\n" + stores[12] + "\n" + stores[13] + "\n" + stores[14] + "\n" + stores[15] + "\n" + stores[16] + "\n" + stores[17] + "\n" + stores[18] + "\n" + stores[19] + "\n" + stores[20] + "\n" + stores[21] + "\n" + stores[22] + "\n" + stores[23] + "\n" + stores[24])
+>>>>>>> 400bf0e7bc4845d519d753066de3450d34c937d0
 
+client.run(TOKEN, reconnect=True)
 
 @client.command(aliases = ['Help', 'HELP'])
 async def help():
